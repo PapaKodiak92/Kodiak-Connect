b410d55ad037a428a2e20ed2e53df98082f233f8
PapaKodiak92 <brisendine.joseph92@gmail.com>
2026-05-31 17:47:19 -0500

    Import acknowledgement modal styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { App } from './app/App';
 import './styles/global.css';
 import './styles/brand.css';
 import './styles/launcher.css';
+import './styles/acknowledgement.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
